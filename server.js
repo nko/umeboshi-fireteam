cef1753b069458332675d19cc0b15d26f49906f2
Fabricio Zuardi <fabricio@187-27-246-166.3g.claro.net.br>
2010-08-26 15:58:33 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//testing Joyent, this is just a copy/paste of http://nodeknockout.posterous.com/countdown-to-knockout-post-11-deploying-to-jo
+// NOT COMPETITION CODE
+var http = require('http');
+ 
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+ 
+server.listen(80);
