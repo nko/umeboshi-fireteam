4b296d648f1e61d9da0c7128981f7b15341b3451
Fabricio Zuardi <fabricio@fcz.local>
2010-08-28 09:11:35 -0300

    checking if port 80 will work as expected on joyent

--- server.js
+++ server.js
@@ -1,75 +1,72 @@
-// var http = require('http');
-//  
-// var server = http.createServer(function (req, res) {
-//   res.writeHead(200, { "Content-Type": "text/plain" })
-//   res.end("Hello world\n");
-// });
-//  
-// server.listen(80);
-//
+var  http = require('http')
+    ,url = require('url')
+    ,fs = require('fs')
+    ,sys = require('sys')
+    ,io = require('./lib/socket.io-node/lib/socket.io');
+    
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
 
-var http = require('http'), 
-		url = require('url'),
-		fs = require('fs'),
-		io = require('./lib/socket.io-node/lib/socket.io'),
-		sys = require('sys'),
-		
 send404 = function(res){
-	res.writeHead(404);
-	res.write('404');
-	res.end();
+  res.writeHead(404);
+  res.write('404');
+  res.end();
 },
-		
+    
 server = http.createServer(function(req, res){
-	// your normal server code
-	var path = url.parse(req.url).pathname;
-	switch (path){
-		case '/':
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write('<h1>Welcome. Try the <a href="/chat.html">chat</a> example.</h1>');
-			res.end();
-			break;
-			
-		default:
-			if (/\.(js|html|swf)$/.test(path)){
-				try {
-				  path = '/lib/socket.io-node/example' + path
-					var swf = path.substr(-4) === '.swf';
-					res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
-					fs.readFile(__dirname + path, swf ? 'binary' : 'utf8', function(err, data){
-						if (!err) res.write(data, swf ? 'binary' : 'utf8');
-						res.end();
-					});
-				} catch(e){ 
-					send404(res); 
-				}
-				break;
-			}
-		
-			send404(res);
-			break;
-	}
+  // your normal server code
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<h1>Welcome. Try the <a href="/chat.html">chat</a> example.</h1>');
+      res.end();
+      break;
+      
+    default:
+      if (/\.(js|html|swf)$/.test(path)){
+        try {
+          path = '/lib/socket.io-node/example' + path
+          var swf = path.substr(-4) === '.swf';
+          res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
+          fs.readFile(__dirname + path, swf ? 'binary' : 'utf8', function(err, data){
+            if (!err) res.write(data, swf ? 'binary' : 'utf8');
+            res.end();
+          });
+        } catch(e){ 
+          send404(res); 
+        }
+        break;
+      }
+    
+      send404(res);
+      break;
+  }
 });
 
 server.listen(8080);
-		
+// server.listen();
+    
 // socket.io, I choose you
 // simplest chat application evar
 var io = io.listen(server),
-		buffer = [];
-		
+    buffer = [];
+    
 io.on('connection', function(client){
-	client.send({ buffer: buffer });
-	client.broadcast({ announcement: client.sessionId + ' connected' });
+  client.send({ buffer: buffer });
+  client.broadcast({ announcement: client.sessionId + ' connected' });
 
-	client.on('message', function(message){
-		var msg = { message: [client.sessionId, message] };
-		buffer.push(msg);
-		if (buffer.length > 15) buffer.shift();
-		client.broadcast(msg);
-	});
+  client.on('message', function(message){
+    var msg = { message: [client.sessionId, message] };
+    buffer.push(msg);
+    if (buffer.length > 15) buffer.shift();
+    client.broadcast(msg);
+  });
 
-	client.on('disconnect', function(){
-		client.broadcast({ announcement: client.sessionId + ' disconnected' });
-	});
+  client.on('disconnect', function(){
+    client.broadcast({ announcement: client.sessionId + ' disconnected' });
+  });
 });
