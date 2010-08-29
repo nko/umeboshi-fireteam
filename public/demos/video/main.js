6813e6326a01d0b2763ab141148a1ae7aaae34fa
Fabricio Zuardi <fabricio@fcz.local>
2010-08-29 16:27:57 -0300

    domain name for use in dev and production

--- public/demos/video/main.js
+++ public/demos/video/main.js
@@ -1,12 +1,22 @@
+//For people without firebug
+if (!console) {
+  var console = {}
+  console.log = function(text){
+    return
+  }
+}
+
 var  channel_key
     ,pubsub_client
-    ,pubsub_server_url = 'umeboshi-fireteam.no.de:8000'
-    ,http_server_url = 'umeboshi-fireteam.no.de';
+    ,domain_name =  window.location.hostname
+    ,pubsub_server_url = 'http://'+domain_name+':8000'
+    ,http_server_url = 'http://'+domain_name + ((domain_name.indexOf('no.de')==-1)?(':8080'):(''));
 
 function init(){
   console.log('init')
+  console.log(domain_name)
   document.getElementById('testlink').addEventListener('click', pubTest, false)
-  pubsub_client = new Faye.Client('http://'+pubsub_server_url+'/pubsubhub', {
+  pubsub_client = new Faye.Client(pubsub_server_url+'/pubsubhub', {
       timeout: 120
   });
 }
@@ -15,7 +25,7 @@ function init(){
 function channelReady(channel){
   channel_key = channel.channel_key;
   console.log('we have a channel, its name is: '+channel.channel_key);  
-  document.getElementById('playback-control-url-field').value = 'http://'+http_server_url+'/demos/video/controls.html?channel='+channel_key;
+  document.getElementById('playback-control-url-field').value = http_server_url+'/demos/video/controls.html?channel='+channel_key;
 }
 
 function pub(obj) {
