7952d46aa6bfb0c9160dd8f8d98764db70d5479a
Fabricio Zuardi <fabricio@fcz.local>
2010-08-29 19:42:31 -0300

    VIDEO DEMO WORKING

--- public/demos/video/controls.js
+++ public/demos/video/controls.js
@@ -9,18 +9,67 @@ if (!console) {
 var  channel_key
     ,pubsub_client
     ,domain_name =  window.location.hostname
-    ,pubsub_server_url = 'http://'+domain_name+':8000';
+    ,pubsub_server_url = 'http://'+domain_name+':8000'
+    ,debug_element
+    ,video_progress_bar
+    ,video_location_bar
+    ,playpausebtn
+    ,playpausebtn_play_img
 
 function init(){
-  console.log(domain_name)
   var querystring = window.location.search
-  var debug_element = document.getElementById('debug-area')
+  debug_element = document.getElementById('debug-area')
+  video_progress_bar = document.getElementById('loaded-bar')
+  video_location_bar = document.getElementById('played-bar')
+  playpausebtn = document.getElementById('play-button')
+  playpausebtn_play_img = document.getElementById('play-image')
+  playpausebtn.addEventListener('click', playPauseVideo, false)
   channel_key = querystring.substring(querystring.indexOf('channel=')+'channel='.length, querystring.length)
-  debug_element.value = channel_key
+  debugLog('Connected on channel:'+channel_key);
   pubsub_client = new Faye.Client(pubsub_server_url+'/pubsubhub', {
       timeout: 120
   });
+  pub({'event':'controls_client_connected'})
   pubsub_client.subscribe('/'+channel_key, function(message) {
-    console.log('Got a message: ' + message.text);
+    switch(message.event){
+      case 'progress':
+        updateLoadingBar({'loaded':message.loaded, 'total':message.total})
+        break;
+      case 'timeupdate':
+        updateTimeBar({'currentTime':message.currentTime, 'duration':message.duration})
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+function pub(obj) {
+  pubsub_client.publish('/'+channel_key, obj);
+}
+
+function updateLoadingBar(e){
+  video_progress_bar.style.width = ((e.loaded/e.total)*100)+'%';
+}
+function updateTimeBar(e){
+  debugLog(JSON.stringify(e))
+  video_location_bar.style.width = ((e.currentTime/e.duration)*100)+'%';
+}
+
+function debugLog(msg){
+  debug_element.value += msg+'\n';
+}
+
+function playPauseVideo(){
+  if (playpausebtn_play_img.style.display == 'block'){
+    playpausebtn_play_img.style.display = 'none'
+    debugLog('play')
+  } else{
+    playpausebtn_play_img.style.display = 'block'
+    debugLog('pause')
+  }
+  pub({
+     'event': 'click'
+    ,'dispatcher': 'playpausebtn'
   });
 }
--- public/demos/video/main.js
+++ public/demos/video/main.js
@@ -16,11 +16,11 @@ var  channel_key
     ,video_location_bar
     ,playpausebtn
     ,playpausebtn_play_img
+    ,control_page_url_element
+    ,display_page_url_element
     
 
 function init(){
-  console.log('init')
-  console.log(domain_name)
   video_element = document.getElementById('main-video'); 
   video_progress_bar = document.getElementById('loaded-bar')
   video_location_bar = document.getElementById('played-bar')
@@ -32,13 +32,27 @@ function init(){
   video_element.addEventListener('progress', updateLoadingBar, false);
   video_element.addEventListener('timeupdate', updateTimeBar, false);
   playpausebtn.addEventListener('click', playPauseVideo, false)
+  pubsub_client.subscribe('/'+channel_key, function(message) {
+    switch(message.event){
+      case 'controls_client_connected':
+        removeControls()
+        break;
+      case 'click':
+        playPauseVideo()
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 
 function channelReady(channel){
+  control_page_url_element = document.getElementById('control-page-url') 
   channel_key = channel.channel_key;
   console.log('we have a channel, its name is: '+channel.channel_key);  
-  document.getElementById('playback-control-url-field').value = http_server_url+'/demos/video/controls.html?channel='+channel_key;
+  // document.getElementById('playback-control-url-field').value = http_server_url+'/demos/video/controls.html?channel='+channel_key;
+  control_page_url_element.innerHTML = http_server_url+'/demos/video/controls.html?channel='+channel_key;
 }
 
 function pub(obj) {
@@ -60,17 +74,40 @@ function pubTest(e){
 
 function updateLoadingBar(e){
   video_progress_bar.style.width = ((e.loaded/e.total)*100)+'%';
+  pub({
+     'event': 'progress'
+    ,'dispatcher': 'video_element'
+    ,'loaded': e.loaded
+    ,'total': e.total
+  });
 }
 function updateTimeBar(e){
-  console.log(e.target.currentTime+' / '+e.target.duration)
   video_location_bar.style.width = ((e.target.currentTime/e.target.duration)*100)+'%';
+  pub({
+     'event': 'timeupdate'
+    ,'dispatcher': 'video_element'
+    ,'currentTime': e.target.currentTime
+    ,'duration': e.target.duration
+  });
+  updatePlayPauseVisual();
 }
-function playPauseVideo(){
+
+function playPauseVideo(e){
+  updatePlayPauseVisual()
   if (video_element.paused){
     video_element.play();
-    playpausebtn_play_img.style.display = 'none'
   } else{
     video_element.pause();
+  }
+}
+function updatePlayPauseVisual(){
+  if (video_element.paused){
+    playpausebtn_play_img.style.display = 'none'
+  } else{
     playpausebtn_play_img.style.display = 'block'
   }
+}
+function removeControls(){
+  var controls = document.getElementById('video-controls')
+  controls.style.display = "none"
 }
