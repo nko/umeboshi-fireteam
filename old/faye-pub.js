99faa913a31602dd473a0141d142cd4ca874f65f
Daniel Quirino Oliveira <danielqo@gmail.com>
2010-09-02 23:22:09 -0300

    removing old stuff

--- old/faye-pub.js
+++
@@ -1,6 +0,0 @@
-var Faye   = require('./lib/faye-0.5.2/faye-node.js')
-var client = new Faye.Client('http://localhost:8000/pubsubhub');
-
-client.publish('/messages', {
-  text: 'Hello world'
-});
--- old/faye-server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-    faye = require('./lib/faye-0.5.2/faye-node.js');
-
-server = new Faye.NodeAdapter({mount: '/pubsubhub'});
-server.listen(8000);
-
-console.log("PubSubHub running at 8000")
-
